9f978daa6c755bc5597fa531d52c5e290dcc03d5
PauloGuizo <paulo_guizo@hotmail.com>
2018-11-30 19:06:03 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/pauloguizo/cursomc/services/CategoriaService.java
+++ src/main/java/com/pauloguizo/cursomc/services/CategoriaService.java
@@ -1,10 +1,13 @@
 package com.pauloguizo.cursomc.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pauloguizo.cursomc.domain.Categoria;
 import com.pauloguizo.cursomc.repositories.CategoriaRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -12,11 +15,19 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
 		
 		Categoria obj = repo.findOne(id);
-		return obj;
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName());
+			  
+		}
+		
+		return obj;			
 		
 	}
+	
+	
 
 }
